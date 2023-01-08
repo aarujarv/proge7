362d011498fd574b84561b32acd4e804b851f60a
aarujarv <a.arujarv@gmail.com>
2023-01-08 19:04:30 +0200

    new

---
+++ nädal9II/Program.cs
@@ -0,0 +1,49 @@
+﻿
+List<string> myShoppingList = GetItemsFromUser();
+ShowItemsFromList(myShoppingList);
+
+
+
+static List<string> GetItemsFromUser()
+{
+    List<string> userList = new List<string>();
+
+    while (true)
+    {
+        Console.WriteLine("Add an item (add)/ Exit (exit):");
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "add")
+        {
+            Console.WriteLine("Enter an item:");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem);
+        }
+
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+        }
+    }
+    return userList;
+}
+
+
+
+
+static void ShowItemsFromList(List<string> someList)
+{
+    Console.Clear();
+
+    int listLen = someList.Count;
+    Console.WriteLine($"You have added {listLen} items to your shopping list.");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+        Console.WriteLine($"{i}.{item}");
+        i++;
+    }
+}
+
